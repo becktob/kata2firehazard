05d69c21b55179a9a9305e6784f6884a5a468a38
Tobias Beckmann <beckmann@sipgate.de>
2024-06-14 09:55:35 +0200

    feat: sort indices, solve instructions
    Co-authored-by: Paul Schwoerer <schwoerer@sipgate.de>

--- fire_hazard.py
+++ fire_hazard.py
@@ -9,16 +9,22 @@ class Grid:
         return sum((sum(row) for row in self._lights))
 
     def turn_on(self, row_range_inclusive: tuple[int, int], col_range_inclusive: tuple[int, int]):
+        row_range_inclusive = sorted(row_range_inclusive)
+        col_range_inclusive = sorted(col_range_inclusive)
         for col in range(col_range_inclusive[0], col_range_inclusive[1] + 1):
             for row in range(row_range_inclusive[0], row_range_inclusive[1] + 1):
                 self._lights[row][col] = True
 
     def turn_off(self, row_range_inclusive: tuple[int, int], col_range_inclusive: tuple[int, int]):
+        row_range_inclusive = sorted(row_range_inclusive)
+        col_range_inclusive = sorted(col_range_inclusive)
         for col in range(col_range_inclusive[0], col_range_inclusive[1] + 1):
             for row in range(row_range_inclusive[0], row_range_inclusive[1] + 1):
                 self._lights[row][col] = False
 
     def toggle(self, row_range_inclusive: tuple[int, int], col_range_inclusive: tuple[int, int]):
+        row_range_inclusive = sorted(row_range_inclusive)
+        col_range_inclusive = sorted(col_range_inclusive)
         for col in range(col_range_inclusive[0], col_range_inclusive[1] + 1):
             for row in range(row_range_inclusive[0], row_range_inclusive[1] + 1):
                 self._lights[row][col] = not self._lights[row][col]
--- test_fire_hazard.py
+++ test_fire_hazard.py
@@ -12,8 +12,18 @@ class TestGrid(TestCase):
         grid = Grid()
 
         grid.turn_on((9, 11), (42, 45))
-
         self.assertEqual(12, grid.count_lit())
+
+    def test_light_turns_on_reverse_indices(self):
+        grid = Grid()
+
+        grid.turn_on((11, 9), (42, 45))
+        self.assertEqual(12, grid.count_lit())
+
+        grid_2 = Grid()
+
+        grid_2.turn_on((11, 9), (45, 42))
+        self.assertEqual(12, grid_2.count_lit())
 
     def test_light_turns_off(self):
         grid = Grid()
@@ -33,3 +43,17 @@ class TestGrid(TestCase):
 
         grid.toggle((5, 5), (5, 12))
         self.assertEqual(96, grid.count_lit())
+
+    def test_instructions(self):
+        grid = Grid()
+        grid.turn_on((887, 9), (959, 629))
+        grid.turn_on((454, 398), (844, 448))
+        grid.turn_off((539, 243), (559, 965))
+        grid.turn_off((370, 819), (676, 868))
+        grid.turn_off((145, 40), (370, 997))
+        grid.turn_off((301, 3), (808, 453))
+        grid.turn_on((351, 678), (951, 908))
+        grid.toggle((720, 196), (897, 994))
+        grid.toggle((831, 394), (904, 860))
+
+        self.assertEqual(100239, grid.count_lit())
