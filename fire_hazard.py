1369fd27523a38a012a086e0d366c110a4754abe
Paul Schwörer <schwoerer@sipgate.de>
2024-06-14 10:06:23 +0200

    refactor: use ints instead of bools internally

--- fire_hazard.py
+++ fire_hazard.py
@@ -6,14 +6,14 @@ class Grid:
         # indices are [row][col]
         rows = 1000
         cols = 1000
-        self._lights = [[False for _ in range(cols)] for _ in range(rows)]
+        self._lights = [[0 for _ in range(cols)] for _ in range(rows)]
 
     def count_lit(self):
         return sum((sum(row) for row in self._lights))
 
     def _apply_operation_to_range(self, row_range_inclusive: tuple[int, int],
                                   col_range_inclusive: tuple[int, int],
-                                  operation: Callable[[bool], bool]):
+                                  operation: Callable[[int], int]):
         row_range_inclusive = sorted(row_range_inclusive)
         col_range_inclusive = sorted(col_range_inclusive)
         for col in range(col_range_inclusive[0], col_range_inclusive[1] + 1):
@@ -21,13 +21,13 @@ class Grid:
                 self._lights[row][col] = operation(self._lights[row][col])
 
     def turn_on(self, row_range_inclusive: tuple[int, int], col_range_inclusive: tuple[int, int]):
-        op = lambda _: True
+        op = lambda _: 1
         self._apply_operation_to_range(row_range_inclusive, col_range_inclusive, op)
 
     def turn_off(self, row_range_inclusive: tuple[int, int], col_range_inclusive: tuple[int, int]):
-        op = lambda _: False
+        op = lambda _: 0
         self._apply_operation_to_range(row_range_inclusive, col_range_inclusive, op)
 
     def toggle(self, row_range_inclusive: tuple[int, int], col_range_inclusive: tuple[int, int]):
-        op = lambda val: not val
+        op = lambda val: 1 if val == 0 else 0
         self._apply_operation_to_range(row_range_inclusive, col_range_inclusive, op)
