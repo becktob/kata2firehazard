e5104dfc2ec0b846fb50d1233081290d6dc49bf8
Paul Schwörer <schwoerer@sipgate.de>
2024-06-14 09:38:44 +0200

    feat: add ability to turn on lights

--- fire_hazard.py
+++ fire_hazard.py
@@ -7,3 +7,8 @@ class Grid:
 
     def count_lit(self):
         return sum((sum(row) for row in self._lights))
+
+    def turn_on(self, row_range_inclusive: tuple[int, int], col_range_inclusive: tuple[int, int]):
+        for col in range(col_range_inclusive[0], col_range_inclusive[1] + 1):
+            for row in range(row_range_inclusive[0], row_range_inclusive[1] + 1):
+                self._lights[row][col] = True
--- test_fire_hazard.py
+++ test_fire_hazard.py
@@ -6,4 +6,12 @@ from fire_hazard import Grid
 class TestGrid(TestCase):
     def test_initialized_grid_is_off(self):
         grid = Grid()
-        self.assertEquals(0, grid.count_lit())
+        self.assertEqual(0, grid.count_lit())
+
+
+    def test_light_turns_on(self):
+        grid = Grid()
+
+        grid.turn_on((9, 11), (42, 45))
+
+        self.assertEqual(12, grid.count_lit())
